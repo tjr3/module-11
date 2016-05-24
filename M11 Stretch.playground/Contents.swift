acb9a75e7cf27b8551564476eedc54b3740b0cd4
Tyler Robinson <tyler.robinson.3@gmail.com>
2016-05-24 09:32:37 -0600

    daily stretch

---
+++ M11 Stretch.playground/Contents.swift
@@ -0,0 +1,57 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+protocol Vehicle {
+    
+    get {
+    var speed: Double
+    var isMoving: Bool
+    }
+    
+    func startMoving()
+    if  isMoving == true {
+    print ("This vehicle is moving")
+    } else { return }
+    
+    
+    func stopMoving()
+    if isMoving == false {
+    print ("This vehicle is stopped")
+    } else { return }
+    
+}
+
+
+class Truck {
+    
+    let speed: Double
+    let isMoving: Bool
+    
+    init(speed: Double, isMoving: Bool) {
+        self.speed = speed
+        self.isMoving = isMoving
+    }
+
+}
+
+class Lawnmower {
+    
+    let speed: Double
+    let isMoving: Bool
+    
+    init(speed: Double, isMoving: Bool) {
+        self.speed = speed
+        self.isMoving = isMoving
+    }
+}
+
+
+let truck = Truck(speed: 60.0, isMoving: true)
+let lawnmower = Lawnmower(speed: 0.0, isMoving: false)
+
+print(Truck)
+
